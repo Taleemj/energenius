19e947129a1cad7b91897067041226909b47ff5f
Taleem <taleemmankuer@gmail.com>
2024-07-22 09:50:25 +0100

    update shadow component

--- components/GlobeShadow.tsx
+++ components/GlobeShadow.tsx
@@ -1,10 +1,10 @@
+import shadow from "../assets/SVG/shadow.svg";
+import Image from "next/image";
+
 const GlobeShadow = () => {
   return (
     <>
-      <div className="fixed z-[-2] top-[55.5%] left-[43%] w-[15vw] h-[15vw] bg-[#131c27fb] rounded-full flat-circle shadow-lg"></div>
-      <div className="fixed z-[-2] top-[45.5%] left-[38%] w-[25vw] h-[25vw] border-[5px] border-blue-500 rounded-full flat-circle shadow-lg"></div>
-      <div className="fixed z-[-3] top-[35.5%] left-[32.5%] w-[35vw] h-[35vw] bg-[#2e64a7] rounded-full flat-circle shadow-lg"></div>
-      <div className="fixed z-[-2] top-[25.5%] left-[27.5%] w-[45vw] h-[45vw] border-[5px] border-blue-500 rounded-full flat-circle shadow-lg"></div>
+      <Image src={shadow} alt="shadow" className="fixed z-[-2] left-[26%] top-[80%] w-[48%]" />
     </>
   );
 };
