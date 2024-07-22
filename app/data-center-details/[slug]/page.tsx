2efc7dea94e3dc9ebf8c9af13916da6da6dbfbc9
Taleem <taleemmankuer@gmail.com>
2024-07-23 00:03:59 +0100

    feat: add on new page

--- app/data-center-details/[slug]/page.tsx
+++ app/data-center-details/[slug]/page.tsx
@@ -24,11 +24,7 @@ const Page = () => {
           <DonutPieChart />
           <RegularPieChart />
         </div>
-        <Card className="bg-dark-blue w-[49.5%] h-full">
-          <CardContent>
-            <p>infomation</p>
-          </CardContent>
-        </Card>
+        <PowerUsageEffectiveness />
       </div>
 
       <div className="flex items-center justify-between w-[75vw] mx-auto my-5">
@@ -45,6 +41,53 @@ const Page = () => {
 };
 
 export default Page;
+
+const PowerUsageEffectiveness = () => {
+  const data: {
+    text: string;
+    value: string;
+  }[] = [
+    {
+      text: "Under utilized servers",
+      value: "10",
+    },
+    {
+      text: "Standard power",
+      value: "800W",
+    },
+    {
+      text: "Ghost servers",
+      value: "14",
+    },
+    {
+      text: "Under utilized servers",
+      value: "16",
+    },
+    {
+      text: "Under utilized servers",
+      value: "25",
+    },
+  ];
+  return (
+    <Card className="bg-dark-blue w-[49.5%] h-full pt-8 pl-4">
+      <CardContent>
+        <h1 className="text-x text-white mb-4">Power Usage Effectiveness</h1>
+        {data.map((item) => (
+          <PowerUsageCards key={item.text} text={item.text} value={item.value} />
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
+
+const PowerUsageCards = ({ text, value }: { text: string; value: string }) => {
+  return (
+    <div className="flex items-center justify-between w-full mb-2">
+      <p className="w-[86%] bg-[#1a2736] px-4 py-3 text-sm">{text}</p>
+      <p className="w-[12%] bg-[#1a2736] px-4 py-3 text-sm">{value}</p>
+    </div>
+  );
+};
 
 const BottomText = ({ amount, text, color }: { amount: string; text: string; color: string }) => {
   return (
