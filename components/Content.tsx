d16aa51a330057bc7ca4c94c88a4ab7582b47418
Taleem <taleemmankuer@gmail.com>
2024-07-23 20:07:16 +0100

    update

--- components/Content.tsx
+++ components/Content.tsx
@@ -14,7 +14,7 @@ const Content: FC<Props> = ({ title }) => {
         defaultValue="Activity"
         className="md:w-[24%] w-[90%] right-auto left-1/2 -translate-x-1/2 md:left-auto md:-translate-x-0 top-[60%] mx-auto absolute md:top-[30%] md:right-[30px]"
       >
-        <TabsList className="w-full flex items-center justify-between bg-dark-blue">
+        <TabsList className="w-full flex items-center justify-between bg-transparent">
           <TabsTrigger
             className="w-[45%] data-[state=active]:bg-button-blue data-[state=active]:text-bright-blue"
             value="Activity"
@@ -49,8 +49,8 @@ export default Content;
 
 const TabsInfo = () => {
   return (
-    <p className="text-white mt-[5px] mb-[15px] text-[14px]">
-      16/5 08:29:57 <span className="text-red-600 font-bold">SGP S02 DO1</span> [ARB control system]{" "}
+    <p className="text-white font-extralight mt-[5px] mb-[15px] text-[14px]">
+      16/5 08:29:57 <span className="text-bright-blue">SGP S02 DO1</span> [ARB control system]{" "}
       <span className="text-yellow-500">Warning cleared</span> m00106 Stat
     </p>
   );
